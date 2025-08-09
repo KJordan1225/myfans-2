73296135e2003140fdd32f9ccab9de268bf356f3
Keith Jordan <shadow902@gmail.com>
2025-08-09 03:39:19 -0400

    add dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,59 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+@section('title')
+    Dashboard
+@endsection
+
+@section('content')
+    <div class="row">
+        <!-- sidebar here -->
+        @include('layouts.components.sidebar')
+        <div class="col-md-9">
+            <h3 class="my-3">Dashboard</h3>
+            <hr />
+            <div class="row mt-2">
+                <div class="col-md-9">
+
+				@if (session('success'))
+					<div class="alert alert-success">
+						{{ session('success') }}
+					</div>
+				@endif
+
+				@if (session('error'))
+					<div class="alert alert-danger">
+						{{ session('error') }}
+					</div>
+				@endif
+
+				
+<div class="py-12" style="margin-left: 250px;">
+	<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+		<div class=" dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+			<div class="p-6 text-gray-900 dark:text-gray-100">
+				{{ __("You're logged in!") }}
+			</div>
+
+			<br>
+			<br>
+			<br>
+			<div>
+				<a href="{{ route('logout') }}" 
+					onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+						Logout
+				</a>
+
+				<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+					@csrf
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
+
