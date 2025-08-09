1d605273fbc88aee4088a4bf9d58ea8da125f6e5
Keith Jordan <shadow902@gmail.com>
2025-08-09 19:03:59 -0400

    update sweetalert2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,9 +15,9 @@
     <body class="bg-light">
         <div class="container-fluid">
             <!-- content here -->
-            @yield('content')
-            @include('sweetalert2::index')
+            @yield('content')            
         </div>
+        @include('sweetalert2::index')
         <!-- Jquery JS -->
         <script
             src="https://code.jquery.com/jquery-3.7.1.min.js"
