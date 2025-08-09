834ce53e8388214ea81eddff07b6e6b9d79f039d
Keith Jordan <shadow902@gmail.com>
2025-08-09 17:55:35 -0400

    update spatie permissions middleware in bootstrap/app.hp

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,33 @@
 <?php
 
+use App\Http\Middleware\CreatorOnly;
 use Illuminate\Foundation\Application;
+use Illuminate\Auth\Middleware\Authenticate;
+use Spatie\Permission\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\Permission\Middleware\PermissionMiddleware;
+use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: [
+            __DIR__.'/../routes/web.php',
+            __DIR__.'/../routes/creator.php',
+             __DIR__.'/../routes/stripe.php',
+            __DIR__.'/../routes/subscription.php',
+        ],
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'creator' => CreatorOnly::class,
+            'auth' => Authenticate::class, // Laravel's default auth middleware is already aliased by default
+            'role' => RoleMiddleware::class,
+            'permission' => PermissionMiddleware::class,
+            'role_or_permission' => RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
